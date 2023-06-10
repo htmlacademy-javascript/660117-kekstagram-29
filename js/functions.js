b7630206524ca4b5896a6d9cae9c005bbdbbc322
DepreKotik <vovachegotov@gmail.com>
2023-06-10 13:01:11 +0300

    провел линтер

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,26 @@
-const count = (string, maxLength) => {
-  return string.length <= maxLength;
-}
+const count = (string, maxLength) => string.length <= maxLength;
 
 const isPalindrom = (string) => {
-  const normalizedString = string.toLowerCase();
+  const normalizedString = string.toLowerCase().
+    replaceAll(' ');
   let newString = '';
-  for (let i=normalizedString.length-1; i>=0 ; i--) {
-    newString +=normalizedString.at(i)
+  for (let i = normalizedString.length - 1; i >= 0 ; i--) {
+    newString += normalizedString[i];
   }
-  return newString === string;
-}
+  return newString === normalizedString;
+};
 
 const takeNumbers = (string) => {
   let number = '';
   if (string >= 0 || string < 0) {
-    string = string.toString()
+    string = string.toString();
   }
-  for (let i=0; i<string.length; i++) {
+  for (let i = 0; i < string.length; i++) {
     if (string[i] >= 0 && string[i] <= 9) {
-      number += string[i]
+      number += string[i];
     } else {
-      continue
+      continue;
     }
   }
-  return parseInt(number)
-}
-
-console.log(takeNumbers(1.5))
+  return parseInt(number);
+};
